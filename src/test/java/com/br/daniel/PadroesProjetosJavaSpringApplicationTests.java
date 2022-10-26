a321b96bcdef64d95b4ef7904570b66b71c4d9e6
Daniel Teixeira <daniellhrt@hotmail.com>
2022-10-26 17:23:04 -0300

    Initial commit

---
+++ src/test/java/com/br/daniel/PadroesProjetosJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.daniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetosJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
